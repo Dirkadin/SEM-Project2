ecc532cb2cf47bba6e0aec1e9e44ccb5bfd7343c
Dirkadin <dirkadin@gmail.com>
2019-11-10 10:48:12 -0600

    updated startup script

--- ordering-service/startup.sh
+++ ordering-service/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Building image
 docker image build -t project2 .
-docker run -d -p 80:3000 -it project2 --mount source=hotburger-logs,target=/ordering-service
+docker run -d -p 80:3000 --mount source=hotburger-logs,target=/ordering-service -it project2
