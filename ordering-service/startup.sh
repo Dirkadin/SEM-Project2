4559c3368e388a8756fb2107347f96bd7ea69d6a
Dirkadin <dirkadin@gmail.com>
2019-11-10 11:06:33 -0600

    .

--- ordering-service/startup.sh
+++ ordering-service/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Building image
 docker image build -t project2 .
-docker run -d -p 80:3000 --mount source=hotburger-logs,target=/ordering-service -it project2
+docker run -d -p 80:3000 --mount type=bind,source=hotburger-logs,target=/ordering-service -it project2
