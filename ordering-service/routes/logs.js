418a76aa7755de967fbb8de0e4b01c91a171748e
Dirkadin <dirkadin@gmail.com>
2019-11-10 11:34:00 -0600

    update

--- ordering-service/routes/logs.js
+++ ordering-service/routes/logs.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const logsPath = {root: path.join(__dirname, "/ordering-service")};
-const log = require('simple-node-logger').createSimpleLogger('/ordering-service/events.log');
+const log = require('simple-node-logger').createSimpleLogger('../ordering-service/events.log');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
