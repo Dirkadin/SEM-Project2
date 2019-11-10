310ed52fa9fad977759db597ffa96adcf15266a8
Dirkadin <dirkadin@gmail.com>
2019-11-10 11:28:41 -0600

    udpates

--- ordering-service/routes/logs.js
+++ ordering-service/routes/logs.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-const logsPath = {root: path.join(__dirname, "../ordering-service/events.log")};
-const log = require('simple-node-logger').createSimpleLogger('logs/events.log');
+const logsPath = {root: path.join(__dirname, "ordering-service")};
+const log = require('simple-node-logger').createSimpleLogger('ordering-service/events.log');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
