1976b8796dec9e0854d4e32df985c04798d49257
Dirkadin <dirkadin@gmail.com>
2019-11-10 13:31:05 -0600

    formatting and cleanup

--- ordering-service/app.js
+++ ordering-service/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var versionRouter = require('./routes/version');
 var getmenuRouter = require('./routes/getmenu');
-var logsRouter = require('./routes/logs');
+//var logsRouter = require('./routes/logs');
 
 const log = require('simple-node-logger').createSimpleLogger('logs/events.log');
 const orders = require('simple-node-logger').createSimpleLogger('logs/orders.log');
@@ -21,21 +21,21 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', indexRouter);
 app.use('/version', versionRouter);
-app.use('/logs', logsRouter);
+//app.use('/logs', logsRouter);
 app.use('/getmenu', getmenuRouter);
 
 app.post('/purchase/:item/:quantity', function (req, res) {
     console.log(req.params.item, req.params.quantity);
-    log.info('POST');
+    log.info('POST /purchase');
     var item = req.params.item.toLowerCase();
     var quantity = req.params.quantity.toLowerCase();
 
@@ -48,25 +48,23 @@ app.post('/purchase/:item/:quantity', function (req, res) {
     }
 
 
-
-
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  log.warn('GET /error');
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    log.warn('GET /error');
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
