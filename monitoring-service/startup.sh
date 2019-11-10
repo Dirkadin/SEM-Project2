6b6482c825f035acbe7e205e207d0e94cdc7ade4
Dirkadin <dirkadin@gmail.com>
2019-11-10 14:33:34 -0600

    updated startup scripts

--- monitoring-service/startup.sh
+++ monitoring-service/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Building image
-docker image build -t ordering-service .
+docker image build -t monitoring-service .
 docker run -d -p 80:3000 --mount type=volume,source=hotburger-logs,target=/app/logs -it project2
